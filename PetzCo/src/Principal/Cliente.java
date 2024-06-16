ef9ebe142602afcf5ea7624cd26a54ef43a57a8f
mandynhaaa <amandatobler18@gmail.com>
2024-06-15 21:00:30 -0300

    fix: corrige erro de update vazio

--- PetzCo/src/Principal/Cliente.java
+++ PetzCo/src/Principal/Cliente.java
@@ -2,18 +2,19 @@ package Principal;
 
 import javax.swing.JOptionPane;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+
 import Connection.SQLGenerator;
+import Padrao.CRUD;
+import Padrao.Formatos;
+import Padrao.Relatorio;
 
 public class Cliente extends Contato implements CRUD {
+	private final String tabela = "cliente";
     private int idCliente;
     private LocalDate dataNascimento;
     
-    private static final DateTimeFormatter DateInputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private static final DateTimeFormatter DateSQLFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
     public int getIdCliente() {
         return idCliente;
     }
@@ -23,12 +24,12 @@ public class Cliente extends Contato implements CRUD {
     }
     
     public String getDataNascimento() {
-        return dataNascimento.format(DateInputFormatter);
+        return dataNascimento.format(Formatos.DATE_INPUT_FORMATTER);
     }
     
     public void setDataNascimento(String dataNascimento) {
         try {
-            this.dataNascimento = LocalDate.parse(dataNascimento, DateInputFormatter);
+            this.dataNascimento = LocalDate.parse(dataNascimento, Formatos.DATE_INPUT_FORMATTER);
         } catch (DateTimeParseException e) {
         	JOptionPane.showMessageDialog(null, "Data de nascimento no formato incorreto. Use dd/MM/yyyy.");
             throw e;
@@ -45,19 +46,18 @@ public class Cliente extends Contato implements CRUD {
 
 	    LocalDate dataNascimento;
 	    try {
-	        dataNascimento = LocalDate.parse(dataNascimentoInput, DateInputFormatter);
+	        dataNascimento = LocalDate.parse(dataNascimentoInput, Formatos.DATE_INPUT_FORMATTER);
 	    } catch (DateTimeParseException e) {
 	        JOptionPane.showMessageDialog(null, "Data de nascimento no formato incorreto. Use dd/MM/yyyy.");
 	        return;
 	    }
 
-	    String dataNascimentoFormatada = dataNascimento.format(DateSQLFormatter);
+	    String dataNascimentoFormatada = dataNascimento.format(Formatos.DATE_SQL_FORMATTER);
 
 	    Endereco endereco = new Endereco();
 	    endereco.cadastrar();
-	    fkEndereco = endereco.getIdEndereco();
-	    
-	    String tabela = "cliente";
+	    int fkEndereco = endereco.getIdEndereco();
+
 	    String colunas = "nome, cpf, telefone, email, dataNascimento, fkEndereco";
 	    String valores = "'" + nome + "','" + cpf + "','" + telefone + "','" + email + "','" 
 	    					+ dataNascimentoFormatada + "', '" + fkEndereco + "'";        
@@ -83,68 +83,68 @@ public class Cliente extends Contato implements CRUD {
 
         int idCliente = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do cliente:"));
         String nome = JOptionPane.showInputDialog("Digite o nome:");
-        if (nome != null) {
+        if (nome != null && !nome.equals("")) {
             colunasList.add("nome");
             valoresList.add(nome);
         }
 
         String cpf = JOptionPane.showInputDialog("Digite o CPF:");
-        if (cpf != null) {
+        if (cpf != null && !cpf.equals("")) {
             colunasList.add("cpf");
             valoresList.add(cpf);
         }
 
         String telefone = JOptionPane.showInputDialog("Digite o telefone:");
-        if (telefone != null) {
+        if (telefone != null && !telefone.equals("")) {
             colunasList.add("telefone");
             valoresList.add(telefone);
         }
 
         String email = JOptionPane.showInputDialog("Digite o e-mail:");
-        if (email != null) {
+        if (email != null && !email.equals("")) {
             colunasList.add("email");
             valoresList.add(email);
         }
 
         String dataNascimentoInput = JOptionPane.showInputDialog("Digite a data de nascimento (dd/MM/yyyy):");
-        if (dataNascimentoInput != null) {
+        if (dataNascimentoInput != null && !dataNascimentoInput.equals("")) {
             LocalDate dataNascimento;
 
             try {
                 colunasList.add("dataNascimento");
-                dataNascimento = LocalDate.parse(dataNascimentoInput, DateInputFormatter);
-                valoresList.add(dataNascimento.format(DateSQLFormatter));
+                dataNascimento = LocalDate.parse(dataNascimentoInput, Formatos.DATE_INPUT_FORMATTER);
+                valoresList.add(dataNascimento.format(Formatos.DATE_SQL_FORMATTER));
             } catch (DateTimeParseException e) {
                 JOptionPane.showMessageDialog(null, "Data de nascimento no formato incorreto. Use dd/MM/yyyy.");
                 return;
             }
         }
-
-        String tabela = "cliente";
         
         Endereco endereco = new Endereco();
         endereco.alterar();
 
-        String[] colunas = colunasList.toArray(new String[0]);
-        String[] valores = valoresList.toArray(new String[0]);
-
-        if (SQLGenerator.updateSQL(tabela, idCliente, colunas, valores)) {
-            JOptionPane.showMessageDialog(null, "Cliente alterado com sucesso!");
-        	setNome(nome);
-        	setCpf(cpf);
-        	setTelefone(telefone);
-        	setEmail(email);
-        	setDataNascimento(dataNascimentoInput);
+        if (!colunasList.isEmpty() && !valoresList.isEmpty()) {
+	        String[] colunas = colunasList.toArray(new String[0]);
+	        String[] valores = valoresList.toArray(new String[0]);
+	
+	        if (SQLGenerator.updateSQL(tabela, idCliente, colunas, valores)) {
+	            JOptionPane.showMessageDialog(null, "Cliente alterado com sucesso!");
+	        	setNome(nome);
+	        	setCpf(cpf);
+	        	setTelefone(telefone);
+	        	setEmail(email);
+	        	setDataNascimento(dataNascimentoInput);
+	        } else {
+	            JOptionPane.showMessageDialog(null, "Ocorreu algum erro na alteração.");
+	        }
         } else {
-            JOptionPane.showMessageDialog(null, "Ocorreu algum erro na alteração.");
+        	JOptionPane.showMessageDialog(null, "Não é possível deixar os dados em branco. Nada alterado.");
         }
     }
     
     @Override
     public void excluir() {
     	int idCliente = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do cliente:"));
-    	
-    	String tabela = "cliente";
     	
     	if (SQLGenerator.deleteSQL(tabela, idCliente)) {
     		JOptionPane.showMessageDialog(null, "Cliente excluído com sucesso!");
@@ -154,16 +154,13 @@ public class Cliente extends Contato implements CRUD {
     }
     
     public void listar() {
-        String tabela = "cliente";
-
         Relatorio.mostrarDados(SQLGenerator.SelectSQL(null, tabela, null, null));
         
     }
     
     public void consultar() {
     	int idCliente = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do cliente:"));
-    	
-    	String tabela = "cliente";
+
     	String colunas = "idCliente, nome, cpf, telefone, email, dataNascimento, idEndereco, "
     					+ "rua, numero, bairro, cidade, estado, complemento, cep";
     	String join = "INNER JOIN endereco ON (endereco.IdEndereco = cliente.fkEndereco)";
